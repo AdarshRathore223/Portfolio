d9badb56296fcb5b69c1e48a64ee434a5f42df7a
Adarsh Rathore <adarshrathore380@gmail.com>
2024-11-19 00:26:59 +0530

    socials bug fixed

--- src/components/Contact/Social.tsx
+++ src/components/Contact/Social.tsx
@@ -8,22 +8,27 @@ function Social() {
     {
       icon: <FaInstagram className="w-12 h-12 inner-element" />,
       delay: "delay-500",
+      link:"https://www.instagram.com/adarshrathore.dev/"
     },
     {
       icon: <CiLinkedin className="w-12 h-12 inner-element" />,
       delay: "delay-700",
+      link:"https://www.linkedin.com/in/adarsh-rathore-80933131b"
     },
     {
       icon: <FiGithub className="w-10 h-10 inner-element" />,
       delay: "delay-1000",
+      link:"https://github.com/AdarshRathore223"
     },
   ];
   return (
     <div className="grid grid-cols-3 gap-3">
       {Technologies.map((item, index) => (
-        <div
+        <a
           className={cn("opacity-0 animate-fade-in-bottom", item.delay)}
           key={index}
+          target="_blank"
+          href={item.link}
         >
           <Tilt
             className="flex justify-center items-center h-20 w-20 parallax-effect bg-card"
@@ -37,7 +42,7 @@ function Social() {
           >
             {item.icon}
           </Tilt>
-        </div>
+        </a>
       ))}
     </div>
   );
